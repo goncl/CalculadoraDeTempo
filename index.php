46aaa4cccc7adc409557e106ab9cd8ed13ca9457
Celso Gonçalves <goncl7@gmail.com>
2025-07-31 01:20:00 -0300

    Primeiro commit - Calculadora de Tempo

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        $total_segundos = $_REQUEST['seg'] ?? '0';
+    ?>
+    <main>
+        <h1>Calculadora de Tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="seg">Qual é o total de segundos?</label>
+            <input type="number" name="seg" id="seg" min="0" step="1" required value="<?=$total_segundos?>">
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <?php 
+        $sobra = $total_segundos;
+
+        $semana = (int)($sobra / 604_800);
+        $sobra = $sobra % 604_800;
+
+        $dia = (int)($sobra / 86_400);
+        $sobra = $sobra % 86_400;
+
+        $hora = (int)($sobra / 3_600);
+        $sobra = $sobra % 3_600;
+
+        $minuto = (int)($sobra / 60);
+        $sobra = $sobra % 60;
+
+        $segundo = $sobra;
+    ?>
+    <section>
+        <h2>Totalizando tudo</h2>
+        <p>Analizando o valor digitado, <?=number_format($total_segundos, 0, ",", ".")?> segundos equivalem a um total de:</p>
+        <ul>
+            <li><?=$semana?> semanas</li>
+            <li><?=$dia?> dias</li>
+            <li><?=$hora?> horas</li>
+            <li><?=$minuto?> minutos</li>
+            <li><?=$segundo?> segundos</li>
+        </ul>
+    </section>
+</body>
+</html>
